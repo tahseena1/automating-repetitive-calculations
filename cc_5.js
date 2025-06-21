2542c2ea75c4a0a2fccc6f00839cb4764583c229
Tahseena Badat <tahseena@usf.edu>
2025-06-21 19:10:46 +0200

    created employee objects

---
+++ cc_5.js
@@ -0,0 +1,6 @@
+let employees = [
+    {name: "Micheal Scott", hourlyRate: 35, hoursWorked: 45},
+    {name: "Jim Halpert", hourlyRate: 25, hoursWorked: 40},
+    {name: "Robert California", hourlyRate: 85, hoursWorked: 15},
+    {name: "Dwight Schrute", hourlyRate: 25, hoursWorked: 60},
+]
