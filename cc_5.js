7786d83d7c4d7f290c978b591f22facf7c81e646
Tahseena Badat <tahseena@usf.edu>
2025-06-21 19:11:56 +0200

    Created function to calculate employee basepay

--- cc_5.js
+++ cc_5.js
@@ -4,3 +4,10 @@ let employees = [
     {name: "Robert California", hourlyRate: 85, hoursWorked: 15},
     {name: "Dwight Schrute", hourlyRate: 25, hoursWorked: 60},
 ]
+
+function calculateBasePay (rate, hours) {
+    if (employees.hoursWorked <= 40)
+        return (rate * hours);
+    else return (rate * 40);
+}
+
