f7f7ff5dbfd1fb2a7dacf37b16ca25579c87ef6f
Tahseena Badat <tahseena@usf.edu>
2025-06-21 19:51:01 +0200

    Logged processPayroll for each employee

--- cc_5.js
+++ cc_5.js
@@ -3,23 +3,23 @@ let employees = [
     {name: "Jim Halpert", hourlyRate: 25, hoursWorked: 40},
     {name: "Robert California", hourlyRate: 85, hoursWorked: 15},
     {name: "Dwight Schrute", hourlyRate: 25, hoursWorked: 60},
-]
+];
 
 function calculateBasePay (rate, hours) {
     if (hours <= 40)
         return (rate * hours);
     else return (rate * 40);
-}
+};
 
 function calculateOvertimePay (rate, hours) {
     if (hours > 40)
         return ((hours - 40) * (rate * 1.5));
     else return 0;
-}
+};
 
 function calculateTaxes (grossPay) {
-    return (grossPay - (.15 * grossPay));
-}
+    return (grossPay - (.15 * grossPay))
+};
 
 function processPayroll(employee) {
     const basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
@@ -32,6 +32,9 @@ function processPayroll(employee) {
         overtimePay: overtimePay.toFixed(2),
         grossPay: grossPay.toFixed(2),
         netPay: netPay.toFixed(2)
-}
-};
+}};
+
+employees.forEach (employee => {
+    console.log(processPayroll(employee))
+});
 
